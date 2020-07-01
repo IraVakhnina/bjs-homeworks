c7b348067dc04cedc97c2a7d190322625400c1b7
Ira Vakhnina <pirania943@gmail.com>
2020-07-01 21:58:16 +0300

    Update task.js

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -1,27 +1,38 @@
- /*
-	Кратко о работе с функциями:
-	1) Значения в скобках - это аргументы, которые мы получаем в момент вызова функции. Их и надо присваивать требуемым в заданиях переменным.
-	2) После ключевого слова return вместо комментария необходимо написать переменную либо выражение с ответом.
-	3) console.log() прописывать не обязательно, т.к. команда return уже означает вывод результата работы функции.
- */
+
+// №1
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+ let algebra = a;
+ let geography = g;
+ let physics = p;
+
+ let average = (a+ g + p);
+ let averageRating = 3;
     
-	return // ваш ответ;
+	return Math.floor(average / averageRating);
+
 }
+
+// №2
 
 function sayHello(userName) {
 	
-    // реализуйте здесь задание №2
+ let myName = userName;
+ let message = `Привет, мир! Меня зовут ${name}`;
     
-	return // ваш ответ;
+	return message + userName;
 }
+
+ //№3
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+  let x = 2;
+  let y = 22;
+  let z = 0;
+  
+  let result = x * y + 5 * z + x - 1;
     
-    return // ваш ответ;
+    return result;
 }
