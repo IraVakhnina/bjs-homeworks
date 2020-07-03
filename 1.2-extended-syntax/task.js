f9bbdb26fc79ee17b0236a3700d3a5a873b3f4fa
Ira Vakhnina <pirania943@gmail.com>
2020-07-03 14:32:27 +0300

    Update task.js

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,46 @@
+'use strict';
+
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    
+    let discriminant = b**2 - 4 * a * c;
+    let x = [];
+
+    if(discriminant == 0){
+        x[0] = -b/(2*a); 
+    }
+    else if(discriminant > 0){
+        x[0] = (-b + Math.sqrt(discriminant))/(2 * a);
+        x[1] = (-b - Math.sqrt(discriminant))/(2 * a);
+    }
+
+    return x;
 }
+
+
+
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+    let number = 0;
+	let averageMark = 0;
+    if (marks.length == 0){
+        return 0;
+    } else if (marks.length > 5){
+        marks.splice(5);
+    }
+    
+    for(let i = 0; i < marks.length; i++){
+        number += marks[i];
+        averageMark = number/marks.length;
+    }
+    return averageMark;
 }
 
+
+
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let nowYear = new Date().getFullYear();
+    let age = nowYear - dateOfBirthday.getFullYear() ;
+    let result = (age >= 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+    return result;
 }
