5cefcb3b48164f1d36e15eecfa1cbdb9e529d777
Ira Vakhnina <pirania943@gmail.com>
2020-07-07 00:49:55 +0300

    Update task.js

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,40 @@
+'use strict';
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+  checkIsNaN(percent, contribution, amount, date);
+  let nowDate = new Date();
+  let numberMonth = countMonth(nowDate, date);
+  let sumCredit = amount - contribution;
+  let p = percent / 100 / 12;
+
+  let payPerMonth = sumCredit * (p + p / (Math.pow(1 + p, numberMonth) - 1));
+
+  let totalAmount = +(numberMonth * payPerMonth).toFixed(2);
+  console.log(totalAmount);
+
+  function countMonth(startDate, endDate) {
+    let months;
+    months = (endDate.getFullYear() - startDate.getFullYear()) * 12;
+    months -= startDate.getMonth();
+    months += endDate.getMonth();
+
+    return months <= 0 ? 0 : months;
+  }
+  function checkIsNaN(prametr, string) {
+    if (!prametr) {
+      return console.log(
+        `Параметр "${string}" содержит неправильное значение ${prametr}`
+      );
+    }
+  }
+
+  return totalAmount;
 }
 
+
+
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+
+  return `Привет, мир! Меня зовут ${name ? name : 'Аноним'}`;
 }
