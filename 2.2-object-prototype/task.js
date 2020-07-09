4f2e059e0ac98a08946297b9a66e801b3b737c7e
Ira Vakhnina <pirania943@gmail.com>
2020-07-09 13:52:07 +0300

    homeworks

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,39 @@
-//String.prototype.isPalindrome - для задачи №1
+//Задача № 1
+
+String.prototype.isPalindrome = function () {
+  const str = this.toString().toLowerCase().replace(/ /g, "");
+
+  const strReverse = [...str].reduce((prev, next) => next + prev);
+
+  let result = str === strReverse ? true : false;
+  return result;
+};
+
+
+
+
+//Задача № 2
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+  let sum;
+  if (marks.length === 0) {
+    return 0;
+  } else {
+    return (sum = Math.round(marks.reduce((x, y) => x + y) / marks.length));
+  }
 }
 
+
+//Задача № 3
+
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+  let now = Math.round(Date.now());
+  birthday = Date.parse(birthday);
+  let diff = now - birthday;
+
+  const userBirthday = 365.25 * 24 * 60 * 60 * 1000;
+  let age = diff / userBirthday;
+  return age > 18 ? true : false;
+
+  //return verdict;
 }
